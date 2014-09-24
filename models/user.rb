58ff32aa8776ab3b9f68f629a79cc6f1337565ee
ericak11 <kantor.erica@gmail.com>
2014-09-24 14:05:23 -0400

    migration created

---
+++ models/user.rb
@@ -0,0 +1,13 @@
+  # primary_key :id
+  # String :name, :size=>127, :null=>false
+  # String :gender, :size=>127, :null=>false
+  # String :picture, :size=>500, :null=>false
+  # String :email, :size=>500, :null=>false
+  # Integer :dob, :null=>false
+  # String :phone, :size=>127, :null=>false
+  # String :location, :size=>500, :null=>false
+  # String :password, :size=>500, :null=>false
+
+  class User < Sequel::Model
+
+  end
