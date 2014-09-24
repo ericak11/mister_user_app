7c5735b16adc98d27a304cd7ce9684921995fcc4
ericak11 <kantor.erica@gmail.com>
2014-09-24 14:00:43 -0400

    migration created

---
+++ db/migrations/001_start.rb
@@ -0,0 +1,15 @@
+Sequel.migration do
+  change do
+    create_table(:users) do
+      primary_key :id
+      String :name, :size=>127, :null=>false
+      String :gender, :size=>127, :null=>false
+      String :picture, :size=>500, :null=>false
+      String :email, :size=>500, :null=>false
+      Integer :dob, :null=>false
+      String :phone, :size=>127, :null=>false
+      String :location, :size=>500, :null=>false
+      String :password, :size=>500, :null=>false
+    end
+  end
+end
